f7ab455037795f3ec4877c2b2c1ea7ee63246f38
Mitheysh Asokan <jeff@Mitheyshs-MacBook-Pro.local>
2018-02-04 13:45:53 -0500

    Sidekiq has been implemented, but it needs work on the response side of things. Look into comments for to remmemeber

--- worker.rb
+++ worker.rb
@@ -1,4 +1,5 @@
 require 'sidekiq'
+require 'lita'
 
 # If your client is single-threaded, we just need a single connection in our Redis connection pool
 Sidekiq.configure_client do |config|
@@ -10,26 +11,14 @@ Sidekiq.configure_server do |config|
   config.redis = {db: 1}
 end
 
- 
-
-module Lita
-  module Handlers
-
-    class Sidekiq < Handler
-      route(/^echo\s+(.+)/, :echo)
-
-      def echo(response)
-        resp = response.matches[0][0].to_s
-        Worker.perform_async(resp)
-        #response.reply(response.matches)
-      end
-
-      Lita.register_handler(self)
-    end
-  end
-end
-
-
 class Worker
   include Sidekiq::Worker
-end 
+  include Lita::Handlers
+
+  def perform(complexity)
+    sleep 4
+    puts "something happened: " + complexity
+    #complexity.reply("hello")
+    #Response.reply(say)
+  end
+end  
